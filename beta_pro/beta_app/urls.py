a2e92e6b5398b45f9412794d098b0465904bedfd
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-11-26 00:15:15 +0530

    beta

--- beta_pro/beta_app/urls.py
+++ beta_pro/beta_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     path('',views.index),
     path('contact',views.contact),
     path('about',views.about),
+    path('course_dtl/<c_id>',views.courses_dtl),
+    path('view_course',views.view_course),
 ]
--- beta_pro/beta_app/views.py
+++ beta_pro/beta_app/views.py
@@ -1,15 +1,47 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
 # Create your views here.
 def index(req):
-    return render(req,'index.html')
-    
+    data=Course.objects.all()[:3]
+    return render(req,'index.html',{'cours':data})
+   
 def contact(req):
     return render(req,'contact.html')
 
 def about(req):
     return render(req,'about.html')
 
-def cour_dtl(req):
-    return render(req,'cors_dtl.html')
+def courses_dtl(req,c_id):
+    data=Course.objects.get(pk=c_id)
+    return render (req,'cors_dtl.html',{'courses':data})
 
+def contact(req):
+    if req.method=='POST':
+        name=req.POST['name']
+        email=req.POST['email']
+        message=req.POST['message']
+        subject=req.POST['subject']
+        phone=req.POST['phone']
+        data=Contact.objects.create(name=name,email=email,message=message,subject=subject,Phone=phone)
+        data.save()
+        return redirect(contact)
+    else:
+        return render (req,'contact.html')
+    
+def view_course(req,):
+    data=Course.objects.all()
+    return render(req,'view_course.html',{'v_courses':data})
+
+def enquiry(req):
+    if req.method=='POST':
+        name=req.POST['name']
+        email=req.POST['email']
+        message=req.POST['message']
+        subject=req.POST['subject']
+        phone=req.POST['phone']
+        data=Contact.objects.create(name=name,email=email,message=message,subject=subject,Phone=phone)
+        data.save()
+        return redirect(enquiry)
+    else:
+        return render (req,'cors_dtl.html')
+    
